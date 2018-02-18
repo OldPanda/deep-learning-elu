79e32a40ce720bbdd6aebc2322499336c193c2fd
prashant47 <prashantgonarkar@gmail.com>
2018-02-18 03:40:39 -0700

    added run script

---
+++ run.py
@@ -0,0 +1,13 @@
+import os
+
+print "Starting ELU Neural Netowrk on MINIST"
+os.system("python elu.py")
+print "ELU Neural Network finished"
+
+print "Starting ReLU Neural Netowrk on MINIST"
+os.system("python relu.py")
+print "ReLU Neural Network finished"
+
+print "Starting Sigmoid Neural Netowrk on MINIST"
+os.system("python sigmoid.py")
+print "sigmoid Neural Network finished"
